6788fa71c2824cac9d7f29caa98b4c7f9170e507
Samuel Grau <samuel.grau@gmail.com>
2011-11-29 05:03:53 +0100

    Nouvelle doc apple doc

--- builddocs.bash
+++ builddocs.bash
@@ -8,10 +8,16 @@ appledoc \
 --project-version 0.8.0 \
 --project-company Vaseltior \
 --company-id com.vaseltior \
+--docset-feed-name "SGBaseFramework Documentation" \
+--docset-feed-url "http://vaseltior.github.com/SGBaseFramework/sgbaseframeworkdocset.atom" \
+--docset-package-url  "http://vaseltior.github.com/SGBaseFramework/com.vaseltior.sgbaseframework.xar" \
 -h \
 -d \
 -n \
 -u \
---output docs/sgbfdocs \
+--keep-undocumented-objects \
+--keep-undocumented-members \
+--search-undocumented-doc \
+--output docs \
 SGBaseFramework
 
